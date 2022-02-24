d6b080a6352ae083b4140f457b1df10a8e637511
Jinx_Dev <62365429+phstutors@users.noreply.github.com>
2022-02-24 10:31:46 -0300

    Add files via upload
    Modificações na index, pfv não mexa!!!

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let produtos =[{nome: "macarrão", codigo: 1234, valor: 123455}];
-const reprovados = produtos.filter(codigoProduto => codigoProduto.codigo == 1234);
-const nomeProduto = reprovados.map(nomeProduto => nomeProduto.nome)
-const valorProduto = reprovados.map(valorProduto => valorProduto.valor);
-console.log(`o valor do produto com o codigo ${reprovados} é valor: ${valorProduto}`)
+
+
+function comprar(){
+    const codigo = document.getElementById('codigo').innerHTML;
+    const codigoFlot = parseFloat(codigo);
+    if(codigoFlot == 1234){
+        const reprovados = this.produtos.filter(codigoProduto => codigoProduto.codigo == 1234);
+        const nomeProduto = reprovados.map(nomeProduto => nomeProduto.nome)
+        const valorProduto = reprovados.map(valorProduto => valorProduto.valor);
+        console.log(`o valor do produto ${nomeProduto} é: ${valorProduto}`)
+    }
+}
